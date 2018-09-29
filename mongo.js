9aef2070b438b57609e7cf196f5a5ee99b400e71
exploringpratik <exploringpratik@gmail.com>
2018-09-29 22:36:52 +0545

    another Commit

--- mongo.js
+++ mongo.js
@@ -4,4 +4,5 @@ const mongoDBErrors = require("mongoose-mongodb-errors");
 
 mongoose.Promise = global.Promise;
 mongoose.plugin(mongoDBErrors);
+console.log(process.env.MONGOURI);
 mongoose.connect(process.env.MONGOURI);
