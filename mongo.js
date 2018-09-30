fee895222a5df9103e5a1e5fac73b7374da39733
exploringpratik <exploringpratik@gmail.com>
2018-09-30 11:12:15 +0545

    another

--- mongo.js
+++ mongo.js
@@ -4,4 +4,4 @@ const mongoDBErrors = require("mongoose-mongodb-errors");
 
 mongoose.Promise = global.Promise;
 mongoose.plugin(mongoDBErrors);
-mongoose.connect(process.env.MONGOURI,{ useNewUrlParser: true });
+mongoose.connect(' mongodb://ghumau123:ghumau123@ds115283.mlab.com:15283/ghumau',{ useNewUrlParser: true });
